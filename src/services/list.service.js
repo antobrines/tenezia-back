1eb9331777a2b9577e804c02943c32c404588023
antobrines <antonin.brines@outlook.fr>
2022-06-06 17:58:17 +0200

    feat: 🎸 change list service to change all position (TODO)

--- src/services/list.service.js
+++ src/services/list.service.js
@@ -1,24 +1,44 @@
-const { list } = require('../models');
+const { List } = require('../models');
 
 const create = async (listBody) => {
-  return await list.create(listBody);
+  return await List.create(listBody);
 };
 
 const getOne = async (id) => {
-  return await list.findById(id);
+  return await List.findById(id);
 };
 
 const updateOne = async (id, listBody) => {
-  return await list.findByIdAndUpdate(id, listBody);
+  return await List.findByIdAndUpdate(id, listBody);
 };
 
 const getAll = async () => {
-  return await list.find({});
+  return await List.find({});
+};
+
+const changePosition = async (id, position) => {
+  await List.find({ position: position }).then(async (list) => {
+    if (list.length > 1) {
+      for (let i = 0; i < list.length; i++) {
+        await List.findByIdAndUpdate(list[i]._id, {
+          position: list[i].position + 1,
+        });
+      }
+    }
+  });
+  return await List.findByIdAndUpdate(
+    id,
+    {
+      position: position,
+    },
+    { new: true }
+  );
 };
 
 module.exports = {
   create,
   getOne,
   updateOne,
-  getAll
+  getAll,
+  changePosition,
 };
