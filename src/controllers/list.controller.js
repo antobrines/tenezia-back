35a58cf32cbc9bb263bda7616b77ca03fd6cd899
antobrines <antonin.brines@outlook.fr>
2022-06-06 17:57:47 +0200

    feat: 🎸 add controller list

---
+++ src/controllers/list.controller.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+const catchAsync = require('../utils/catchAsync');
+const listService = require('../services/list.service');
+const { errorF, successF } = require('../utils/message');
+const httpStatus = require('http-status');
+
+const create = catchAsync(async (req, res, next) => {
+  const listCreated = await listService.create(req.body);
+  successF('List Created', listCreated, httpStatus.CREATED, res, next);
+});
+
+const update = catchAsync(async (req, res, next) => {
+  const listUpdated = await listService.update(req.params.id, req.body);
+  successF('List Updated', listUpdated, httpStatus.OK, res, next);
+});
+
+const changePosition = catchAsync(async (req, res, next) => {
+  const listUpdated = await listService.changePosition(
+    req.params.id,
+    req.body.position
+  );
+  successF('List position Updated', listUpdated, httpStatus.OK, res, next);
+});
+
+const getAll = catchAsync(async (req, res, next) => {
+  const lists = await listService.getAll();
+  successF('Lists', lists, httpStatus.OK, res, next);
+});
+
+module.exports = {
+  create,
+  update,
+  changePosition,
+  getAll,
+};
