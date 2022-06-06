f5abcb28ad3ae5b0a7ad34a6a758410cb0fbac8c
antobrines <antonin.brines@outlook.fr>
2022-06-06 17:05:15 +0200

    feat: 🎸 add list service

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,4 @@
 module.exports.User = require('./user.model');
+module.exports.Question = require('./question.model');
+module.exports.Answer = require('./answer.model');
+module.exports.List = require('./list.model');
---
+++ src/services/list.service.js
@@ -0,0 +1,24 @@
+const { list } = require("../models");
+
+const create = async (listBody) => {
+  return await list.create(listBody);
+};
+
+const getOne = async (id) => {
+  return await list.findById(id);
+};
+
+const updateOne = async (id, listBody) => {
+  return await list.findByIdAndUpdate(id, listBody);
+};
+
+const getAll = async () => {
+  return await list.find({});
+};
+
+module.exports = {
+  create,
+  getOne,
+  updateOne,
+  getAll
+};
