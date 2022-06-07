9a3a06e6501e52a6b01585d5a694b88772428832
antobrines <antonin.brines@outlook.fr>
2022-06-07 09:39:01 +0200

    feat: 🎸 add changePosition

--- src/services/list.service.js
+++ src/services/list.service.js
@@ -17,22 +17,24 @@ const getAll = async () => {
 };
 
 const changePosition = async (id, position) => {
-  await List.find({ position: position }).then(async (list) => {
-    if (list.length > 1) {
-      for (let i = 0; i < list.length; i++) {
-        await List.findByIdAndUpdate(list[i]._id, {
-          position: list[i].position + 1,
+  const listUpdate = await List.findById(id);
+  if (listUpdate.position > position) {
+    position = position - 0.1;
+  } else {
+    position = position + 0.1;
+  }
+  listUpdate.position = position;
+  await listUpdate.save();
+  await List.find({})
+    .sort({ position: 1 })
+    .exec((err, lists) => {
+      if (err) throw err;
+      lists.forEach((list, index) => {
+        List.findByIdAndUpdate(list._id, { position: index + 1 }, (err) => {
+          if (err) throw err;
         });
-      }
-    }
-  });
-  return await List.findByIdAndUpdate(
-    id,
-    {
-      position: position,
-    },
-    { new: true }
-  );
+      });
+    });
 };
 
 module.exports = {
